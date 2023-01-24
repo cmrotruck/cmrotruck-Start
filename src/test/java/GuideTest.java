cff623063e86b07cd5486239e5d5a14a6f470860
Chad Rotruck <crotruck@gmail.com>
2023-01-24 01:14:00 +0000

    done

--- src/test/java/GuideTest.java
+++ src/test/java/GuideTest.java
@@ -2,7 +2,7 @@
  * Welcome to your first lab! This lab will identify all the different entities that you'll be creating and
  * manipulating in your code. It is purely informational, and you do NOT need to write or change anything in this
  * lab. The test cases for this lab will pass from the start.
- *
+ * 
  * This file is a CLASS. A CLASS is a blueprint for generating an OBJECT. For now, you can think of a
  * Class as its own program, whose functionality gets executed whenever one of its methods gets used. A method is
  * something that an Object can 'do'. Objects contain states (variables) and behavior (methods).
